95690947515d89e0cee1dc2bab935906945c4a0a
yuxianGG <940114763@qq.com>
2019-09-02 16:01:48 +0800

    完成首页搜索栏

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,38 @@
         />
       </el-carousel-item>
     </el-carousel>
+
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row
+          type="flex"
+          class="search-tab"
+        >
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active:current===index}"
+            @click="handleClick(index)"
+          >
+            <i>{{ item.title }}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row
+          type="flex"
+          align="middle"
+          class="search-input"
+        >
+          <input
+            :placeholder="options[current].placeholder"
+          >
+          <i class="el-icon-search" />
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,7 +53,21 @@ export default {
         // 'http://157.122.54.189:9095/assets/images/th01.jfif',
         // 'http://157.122.54.189:9095/assets/images/th02.jfif',
         // 'http://157.122.54.189:9095/assets/images/th03.jfif'
-      ]
+      ],
+      // 定义tab切换的数据
+      options: [
+        { title: '攻略',
+          placeholder: '请输入城市'
+        },
+        { title: '酒店',
+          placeholder: '请输入城市搜索酒店'
+        },
+        { title: '机票',
+          placeholder: ''
+        }
+      ],
+      // 当前的索引
+      current: 0
     }
   },
   mounted () {
@@ -34,6 +80,17 @@ export default {
       const data = res.data.data
       this.banners = data
     })
+  },
+  methods: {
+    // 点击搜索tab栏时触发
+    handleClick (index) {
+      // 点击索引是2的时候代表是机票
+      if (index === 2) {
+        this.$router.push('/air')
+      }
+      // 把当前点击的索引赋值给current
+      this.current = index
+    }
   }
 }
 </script>
@@ -50,6 +107,87 @@ export default {
     .banner-image{
         width:100%;
         height:100%;
+    }
+
+    .banner-content{
+        z-index:9;
+        width:1000px;
+        position:absolute;
+        left:50%;
+        top:45%;
+        margin-left: -500px;
+        border-top:1px transparent solid;
+        .search-bar{
+            width:552px;
+            margin:0 auto;
+        }
+        .search-tab{
+            .active{
+                i{
+                color:#333;
+                }
+                &::after{
+                background: #eee;
+                }
+            }
+            span{
+                width:82px;
+                height:36px;
+                display:block;
+                position: relative;
+                margin-right:8px;
+                cursor: pointer;
+                i{
+                position:absolute;
+                z-index:2;
+                display: block;
+                width:100%;
+                height:100%;
+                line-height:30px;
+                text-align:center;
+                color:#fff;
+                }
+                &:after{
+                position: absolute;
+                left:0;
+                top:0;
+                display:block;
+                content: "";
+                width:100%;
+                height:100%;
+                border: 1px rgba(255,255,255,.2) solid;
+                border-bottom: none;
+                transform: scale(1.1,1.3) perspective(.7em) rotateX(2.2deg);
+                transform-origin: bottom left;
+                background: rgba(0,0,0,.5);
+                border-radius:1px 2px 0 0;
+                box-sizing:border-box;
+                }
+            }
+        }
+        .search-input{
+            width:550px;
+            height:46px;
+            background:#fff;
+            border-radius: 0 4px 4px 4px;
+            border: 1px rgba(255,255,255,.2) solid;
+            border-top:none;
+            box-sizing: unset;
+            input{
+                flex:1;
+                height:20px;
+                padding: 13px 15px;
+                outline: none;
+                border:0;
+                font-size:16px;
+            }
+            .el-icon-search{
+                cursor :pointer;
+                font-size:22px;
+                padding:0 10px;
+                font-weight:bold;
+            }
+        }
     }
 }
 </style>
